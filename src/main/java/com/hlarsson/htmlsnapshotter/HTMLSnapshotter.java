f2f749119d46f2d87993ac85c13cb6c0d488fb3b
Henrik Larsson <henrik@hlarsson.com>
2013-12-16 16:13:12 +0100

    Simple html snapshot tool

---
+++ src/main/java/com/hlarsson/htmlsnapshotter/HTMLSnapshotter.java
@@ -0,0 +1,48 @@
+package com.hlarsson.htmlsnapshotter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HTMLSnapshotter extends HttpServlet {
+    private static final String ESCAPE_FRAGMENT = "fragment";
+    private static final String URL = "url";
+
+    public void init() throws ServletException {
+    }
+ 
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        if (request.getParameter(URL) != null && request.getParameter(ESCAPE_FRAGMENT) != null) {
+            String url = URLDecoder.decode(
+                    request.getParameter(URL) + "#!" + request.getParameter(ESCAPE_FRAGMENT),
+                    "UTF-8");
+
+            final WebClient webClient = new WebClient();
+            HtmlPage page = (HtmlPage) webClient.getPage(url);
+
+            // important!  Give the headless browser enough time to execute JavaScript
+            // The exact time to wait may depend on your application.
+            int jobs = webClient.waitForBackgroundJavaScript(5000);
+
+            out.println(page.asXml());
+        } else {
+            out.write("<html><head><title>Missing parameter</title><body><p>" +
+                    "Missing parameter " + URL + ".</p></body></html>");
+        }
+    }
+  
+    public void destroy() {
+    }
+}
